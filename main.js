3541ea52ed762b956447cfd7ad0c6ed13ac09747
landal-james <127319168+landal-james@users.noreply.github.com>
2024-02-27 14:17:23 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+
 function mudarCorTexto()
 {
-    alert("Hello World")
+    document.querySelector("#p1").style.color= "green"
+}
+
+function mudarCorFundo(){
+    document.querySelector("#p2").style.background= "purple"
+}
+
+function alterarTexto(){
+
+    textoForm = document.querySelector("#textinput").value
+ 
+    document.querySelector("#texto2").innerHTML = textoForm
+
 }
